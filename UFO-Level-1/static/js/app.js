57249de04c093fa184aaf6cfd1dcfbd8018e5844
samuelokunola326 <samuelokunola@gmail.com>
2020-11-21 02:45:08 -0500

    updating app.js file for level one ufo data

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -9,19 +9,6 @@ var tbody = d3.select("tbody");
 // console.log the ufo data 
 console.log(tableData);
 
-// // loop through data and console.log ufoData
-// tableData.forEach(function(ufoData){
-//     console.log(ufoData);
-// });
-
-
-// // using d3 to append tr element for each object in ufoData
-// tableData.forEach(function(ufoData){
-//     console.log(ufoData);
-//     var row = tbody.append("tr");
-// });
-
-
 // looping through the table data
 tableData.forEach((ufoData) => {
     // using d3 to append a tr element for each Object in the Array of objects 
